51791b086710993a743db004332e9d71261ec0b1
Sergo-python <shytuku@ukr.net>
2021-08-26 19:54:06 +0300

    JS les2

---
+++ JS/les2/les2.js
@@ -0,0 +1,13 @@
+1.  const group = [0, 1, 2, 3, 4];
+    console.log(group);
+    const group1 = ['A', 'B', 'C', 'D', 'E'];
+    console.log(group1);
+    const group2 = ['String', false, true, 100, 10];
+    console.log(group2);
+
+2.  const group = [false, true, 100];
+    console.log(group[1]);
+    console.log(group[2]);
+    console.log(group[0]);
+
+ВСЁ ЧТО ИДЕТ ДАЛЬШЕ - Я НЕ ЗНАЮ КАК ДЕЛАТЬ.НЕ БЫЛО ТЕМЫ КАК ТАКОВОЙ ЛИБО Я ПРОСТО ТУПОЙ)
